b2e69f178eb531e0550cb6d70545e434b5d1fde4
Worameth Suwannapruuk <nut.worameth@gmail.com>
2023-05-07 15:48:07 +0700

    part1

--- controller/itemsController.js
+++ controller/itemsController.js
@@ -16,6 +16,7 @@ exports.getGroupMembers = async (req, res) => {
   };
   try {
     const data = await docClient.send(new ScanCommand(params));
+    // console.log(data);
     res.send(data.Items);
   } catch (err) {
     console.error(err);
@@ -25,18 +26,37 @@ exports.getGroupMembers = async (req, res) => {
 
 // TODO #1.1: Get items from DynamoDB
 exports.getItems = async (req, res) => {
-  // You should change the response below.
-  res.send("This route should get all items in DynamoDB.");
+  const params = {
+    TableName: process.env.aws_items_table_name,
+  };
+  try{
+    const data = await docClient.send(new ScanCommand(params));
+    res.send(data.Items);
+  }catch(err){
+    console.error(err);
+    res.status(500).send(err);
+  }
+  
 };
 
 // TODO #1.2: Add an item to DynamoDB
 exports.addItem = async (req, res) => {
   const item_id = uuidv4();
   const created_date = Date.now();
-  const item = { item_id: item_id, ...req.body, created_date: created_date };
-
+  const item = { items_id: item_id , ...req.body , created_date: created_date };
   // You should change the response below.
-  res.send("This route should add an item in DynamoDB.");
+  params = {
+    TableName: process.env.aws_items_table_name,
+    Item: item
+  }
+  try{
+    const data = await docClient.send(new PutCommand(params));
+    res.send(data);
+  }catch(err){
+    console.error(err);
+    res.status(500).send(err);
+  }
+
 };
 
 // TODO #1.3: Delete an item from DynamDB
@@ -44,5 +64,17 @@ exports.deleteItem = async (req, res) => {
   const item_id = req.params.item_id;
 
   // You should change the response below.
-  res.send("This route should delete an item in DynamoDB with item_id.");
+  const params ={
+    TableName: process.env.aws_items_table_name,
+    Key: {
+      items_id : item_id
+    }
+  }
+  try{
+    const data = await docClient.send(new DeleteCommand(params));
+    res.send(data);
+  }catch(err){
+    console.error(err);
+    res.status(500).send(err);
+  }
 };
